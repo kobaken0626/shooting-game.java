8f370a86a9ea8a2f1434d428ae9bad3641b41019
RX-0 <zeal23310160@fsgsl.jp>
2024-06-11 12:15:40 +0900

    Java 10.3 基本となるクラスを作成する

--- Shooting/src/Player.java
+++ Shooting/src/Player.java
@@ -1,4 +1,10 @@
 
-public class Player {
+public class Player extends Character{
+
+	public Player(double x, double y, double vx, double vy) {
+		//Characterクラスのコンストラクタ呼び出し(8章)
+		super(x, y, vx, vy);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
 
 }
