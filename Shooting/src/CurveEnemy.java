aca0fa0c761bb4594843bf8be21e9dae45bc319a
RX-0 <zeal23310160@fsgsl.jp>
2024-06-24 11:11:18 +0900

    ※マージミス

--- Shooting/src/CurveEnemy.java
+++ Shooting/src/CurveEnemy.java
@@ -8,7 +8,7 @@ public class CurveEnemy extends Enemy{
 	}
 	public CurveEnemy(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
-        life=3;
+		life=3;
 	}
 	public void move() {
 		super.move();
