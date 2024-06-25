f6c32c4658a86255e9a0bdb2a31a530c8802d46c
RX-0 <zeal23310160@fsgsl.jp>
2024-06-25 10:35:10 +0900

    練習問題 13-1

--- Shooting/src/DropEnemy.java
+++ Shooting/src/DropEnemy.java
@@ -8,10 +8,10 @@ public class DropEnemy extends Enemy{
 	}
 	public DropEnemy(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
-		life=1;
+		life=1+GameWorld.stage; 
 	}
 	public void move() {
 		super.move();
-		vy=vy+0.1;
+		vy=(vy+0.1)+GameWorld.stage;
 	}
 }
--- Shooting/src/RandomEnemy.java
+++ Shooting/src/RandomEnemy.java
@@ -6,14 +6,14 @@ public class RandomEnemy extends Enemy{
 		f.setColor(0, 128, 0);
 		f.fillRect(x+20,y,10,20);
 		f.setColor(0, 128, 0);
-		f.fillRect(x+10,y+20,10,10);
+		f.fillRect(x+10,y+20,10,10); 
 	}
 	public RandomEnemy(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
-		life=2;
+		life=2+GameWorld.stage;
 	}
 	public void move() {
 		super.move();
-		vx=Math.random()*4-2;
+		vx=Math.random()*(3-GameWorld.stage)-2;
 	}
 }
